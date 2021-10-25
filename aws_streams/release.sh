459ff3b361d0da5b55758699cadff94c6cb9ca1d
jvanbrie <jon.vanbriesen@datadoghq.com>
2021-10-25 16:51:57 -0400

    fixing release script

--- aws_streams/release.sh
+++ aws_streams/release.sh
@@ -32,9 +32,9 @@ fi
 
 # Update bucket placeholder
 # Use datadog-cloudformation-stream-template as the s3 template for production
-cp main.yaml main.yaml.bak
-perl -pi -e "s/<BUCKET_PLACEHOLDER>/${BUCKET}/g" main.yaml
-trap 'mv main.yaml.bak main.yaml' EXIT
+cp streams_main.yaml streams_main.yaml.bak
+perl -pi -e "s/<BUCKET_PLACEHOLDER>/${BUCKET}/g" streams_main.yaml
+trap 'mv streams_main.yaml.bak streams_main.yaml' EXIT
 
 # Upload
 if [ "$PRIVATE_TEMPLATE" = true ] ; then
@@ -44,6 +44,6 @@ else
         --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 fi
 echo "Done uploading the template, and here is the CloudFormation quick launch URL"
-echo "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-aws-streams&templateURL=https://${BUCKET}.s3.amazonaws.com/aws/main.yaml"
+echo "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-aws-streams&templateURL=https://${BUCKET}.s3.amazonaws.com/aws/streams_main.yaml"
 
 echo "Done!"
