6ced39f5ef2f4b0ce7f7f4f614d3260c62c5a767
Ashwin Venkatesan <ashwin.venkatesan@datadoghq.com>
2024-08-28 11:08:45 -0400

    add readme and release script

---
+++ aws_cloud_cost/release.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env bash
+
+# Usage: ./release.sh <S3_Bucket>
+
+set -e
+
+# Read the S3 bucket
+if [ -z "$1" ]; then
+    echo "Must specify a S3 bucket to publish the template"
+    exit 1
+else
+    BUCKET=$1
+fi
+
+# Upload templates to a private bucket -- useful for testing
+if [[ $# -eq 2 ]] && [[ $2 = "--private" ]]; then
+    PRIVATE_TEMPLATE=true
+else
+    PRIVATE_TEMPLATE=false
+fi
+
+# Confirm to proceed
+for i in *.yaml; do
+    [ -f "$i" ] || break
+    echo "About to upload $i to s3://${BUCKET}/aws/$i"
+done
+read -p "Continue (y/n)?" CONT
+if [ "$CONT" != "y" ]; then
+  echo "Exiting"
+  exit 1
+fi
+
+# Update bucket placeholder
+cp datadog_cloud_cost_setup.yaml datadog_cloud_cost_setup.yaml.bak
+perl -pi -e "s/<BUCKET_PLACEHOLDER>/${BUCKET}/g" datadog_cloud_cost_setup.yaml
+trap 'mv datadog_cloud_cost_setup.yaml.bak datadog_cloud_cost_setup.yaml' EXIT
+
+# Upload
+if [ "$PRIVATE_TEMPLATE" = true ] ; then
+    aws s3 cp . s3://${BUCKET}/aws --recursive --exclude "*" --include "*.yaml"
+else
+    aws s3 cp . s3://${BUCKET}/aws --recursive --exclude "*" --include "*.yaml" \
+        --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+fi
+echo "Done uploading the template, and here is the CloudFormation quick launch URL"
+echo "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-cloud-cost-setup&templateURL=https://${BUCKET}.s3.amazonaws.com/aws/datadog_cloud_cost_setup.yaml"
+
+echo "Done!"
