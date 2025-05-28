0bcf7f0b7df6c2b4e753192b3a03bc1d3c126b5d
Diogo Pereira <diogo.pereira@datadoghq.com>
2025-05-28 17:16:03 +0100

    Add agentless API call to delegate role template (#212)
    * Add agentless API call template
    * Move Python code to separate file
    * Add Agentless API call to delegate role template
    * Bump aws_quickstart version to 2.1.13

--- aws_quickstart/release.sh
+++ aws_quickstart/release.sh
@@ -55,7 +55,21 @@ cp main_extended.yaml main_extended.yaml.bak
 perl -pi -e "s/<BUCKET_PLACEHOLDER>/${BUCKET}/g" main_extended.yaml
 perl -pi -e "s/<VERSION_PLACEHOLDER>/${VERSION}/g" main_extended.yaml
 
-trap 'mv main_v2.yaml.bak main_v2.yaml; mv main_extended.yaml.bak main_extended.yaml' EXIT
+# Replace ZIPFILE_PLACEHOLDER with the contents of the Python file
+perl -i.bak -pe '
+    # Read the Python script from stdin
+    BEGIN { $p = do { local $/; <STDIN> } }
+    # Find the placeholder and capture its indentation
+    /^(\s+)<ZIPFILE_PLACEHOLDER>/ && (
+        # Replace with the Python script, preserving the indentation
+        $_ = join("\n", map { $1 . $_ } split(/\n/, $p)) . "\n"
+    )
+' datadog_agentless_delegate_role.yaml < datadog_agentless_api_call.py
+
+trap 'mv main_v2.yaml.bak main_v2.yaml;
+      mv main_extended.yaml.bak main_extended.yaml;
+      mv datadog_agentless_delegate_role.yaml.bak datadog_agentless_delegate_role.yaml;
+' EXIT
 
 # Upload
 if [ "$PRIVATE_TEMPLATE" = true ] ; then
