1ed9d8b7f38ea05e6b01b47c31192b305b101e1f
Moez Ezzeddine <moez.ezzeddine@datadoghq.com>
2025-10-21 13:46:19 +0200

    Update release.sh to make it update the stackset in case the version changes. Also prints a warning

--- aws_quickstart/release.sh
+++ aws_quickstart/release.sh
@@ -72,11 +72,27 @@ for template in datadog_agentless_delegate_role.yaml datadog_agentless_scanning.
     ' "$template" < datadog_agentless_api_call.py
 done
 
+# Update TemplateURL version in aws_organizations/main_organizations.yaml
+ORGANIZATIONS_TEMPLATE="../aws_organizations/main_organizations.yaml"
+ORGANIZATIONS_VERSION_UPDATED=false
+if [ -f "$ORGANIZATIONS_TEMPLATE" ]; then
+    # Check if the version needs updating
+    CURRENT_ORG_VERSION=$(grep -oP 'https://datadog-cloudformation-template-quickstart\.s3\.amazonaws\.com/aws/\Kv[0-9]+\.[0-9]+\.[0-9]+(?=/datadog_agentless_scanning\.yaml)' "$ORGANIZATIONS_TEMPLATE" || echo "")
+    if [ -n "$CURRENT_ORG_VERSION" ] && [ "$CURRENT_ORG_VERSION" != "$VERSION" ]; then
+        ORGANIZATIONS_VERSION_UPDATED=true
+        echo "Updating main_organizations.yaml TemplateURL from $CURRENT_ORG_VERSION to $VERSION"
+    fi
+    cp "$ORGANIZATIONS_TEMPLATE" "$ORGANIZATIONS_TEMPLATE.bak"
+    # Update the version in the TemplateURL for datadog_agentless_scanning.yaml
+    perl -pi -e "s|(https://datadog-cloudformation-template-quickstart\.s3\.amazonaws\.com/aws/)v[0-9]+\.[0-9]+\.[0-9]+(/datadog_agentless_scanning\.yaml)|\$1${VERSION}\$2|g" "$ORGANIZATIONS_TEMPLATE"
+fi
+
 trap 'mv main_v2.yaml.bak main_v2.yaml;
       mv main_extended.yaml.bak main_extended.yaml;
       mv datadog_agentless_scanning.yaml.bak datadog_agentless_scanning.yaml;
       mv datadog_agentless_delegate_role.yaml.bak datadog_agentless_delegate_role.yaml;
       mv datadog_agentless_delegate_role_snapshot.yaml.bak datadog_agentless_delegate_role_snapshot.yaml;
+      mv "'$ORGANIZATIONS_TEMPLATE'.bak" "'$ORGANIZATIONS_TEMPLATE'";
 ' EXIT
 
 # Upload
@@ -88,5 +104,11 @@ else
 fi
 echo "Done uploading the template, and here is the CloudFormation quick launch URL"
 echo "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?stackName=datadog-aws-integration&templateURL=https://${BUCKET}.s3.amazonaws.com/aws/${VERSION}/main_v2.yaml"
+
+if [ "$ORGANIZATIONS_VERSION_UPDATED" = true ]; then
+    echo ""
+    echo "⚠️  WARNING: main_organizations.yaml has been updated with the new version ($VERSION)."
+    echo "   You need to release aws_organizations as well by running: cd ../aws_organizations && ./release.sh"
+fi
 
 echo "Done!"
